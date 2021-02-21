769da31bf7418ab64242fc339c524f975950089f
Thefriendlymoose <alamiridavid@gmail.com>
2021-02-21 20:35:03 +0100

    project finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,26 @@
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0; 
+}
+
 //VARIABLES
 let chosenNumber = 0;
 let previousNumber;
 let total;
+let currentOperator;
+let previousOperator;
 
-let decimalUsed = 0;
+
 
 const numbers = document.querySelectorAll(".number-button");
-const operators = document.querySelectorAll(".non-number-button");
+const operators = document.querySelectorAll(".operator");
 const entry = document.getElementById("entry");
 const entryHist = document.getElementById("entry-history");
+const negateButton = document.getElementById("negate");
+const clearButton = document.getElementById("clear");
+const backButton = document.getElementById("back")
+const sqrtButton = document.getElementById("sqr");
 
-console.log(numbers);
 
 //INIT
 entry.value = chosenNumber;
@@ -19,16 +29,18 @@ entry.value = chosenNumber;
 //EVENTLISTENERS
 numbers.forEach((button) => {
     button.addEventListener("click", (event) => {
-        if(event.target.value === "."){
-            decimalUsed++;
-        }
-
-        if(decimalUsed === 1){
-            chosenNumber += event.target.value;
-            entry.value += event.target.value;
-            decimalUsed++;
-        } else if(!(event.target.value === ".")){
-            if(chosenNumber === 0){
+        if(String(chosenNumber).indexOf(".") < 0 && event.target.value === "."){
+            console.log("wrong");
+            if(chosenNumber === undefined || chosenNumber === 0 || chosenNumber === "0"){
+                chosenNumber = 0;
+                chosenNumber += event.target.value;
+                entry.value += event.target.value;
+            } else{
+                chosenNumber += event.target.value;
+                entry.value += event.target.value;
+            }
+        } else if(!(event.target.value === ".")) {
+            if(chosenNumber === 0 || chosenNumber === "0" || chosenNumber === undefined){
                 chosenNumber = event.target.value;
                 entry.value = event.target.value;
             } else{
@@ -42,52 +54,202 @@ numbers.forEach((button) => {
 
 operators.forEach((button) =>{
     button.addEventListener("click", (event) => {
-        if(event.target.value === "plus"){
-            entryHist.value += `${chosenNumber} + `;
-            previousNumber = chosenNumber;
-            chosenNumber = 0;
-        } else if(event.target.value === "minus"){
-            entryHist.value += `${chosenNumber} - `;
-            previousNumber = chosenNumber;
-            chosenNumber = 0;
-        } else if(event.target.value === "multi"){
-            entryHist.value += `${chosenNumber} x `;
-            previousNumber = chosenNumber;
-            chosenNumber = 0;
-        }else if(event.target.value === "divide"){
-            entryHist.value += `${chosenNumber} / `;
-            previousNumber = chosenNumber;
-            chosenNumber = 0;
+        if(entryHist.value.indexOf("=") > 0 && event.target.value === "="){
+            //chosenNumber = 0;
+            entryHist.value = `${total} ${previousOperator} ${previousNumber} ${currentOperator}`
+            total = operation(currentOperator, total, previousNumber);
+            entry.value = total;
+            
+        } else if(entryHist.value.indexOf("=") > 0 && event.target.value !== "="){
+            currentOperator = event.target.value;
+            previousOperator = currentOperator;
+            entryHist.value = `${total} ${event.target.value} `;
+            previousNumber = total;
+        } else if(chosenNumber === undefined){
+            currentOperator = event.target.value;
+            entryHist.value = entryHist.value.slice(0, -2);
+            entryHist.value += `${currentOperator} `
         }
+        
+        else{
+            if(previousNumber !== undefined & currentOperator !== undefined){
+                if(total !== undefined){
+                    total = operation(currentOperator, total, chosenNumber);
+                    previousNumber = chosenNumber;
+                    entryHist.value += `${chosenNumber} ${event.target.value} `;
+                    chosenNumber = undefined;
+                    entry.value = total;
+                    previousOperator = currentOperator;
+                    currentOperator = event.target.value;
+                } else{
+                    total = operation(currentOperator, previousNumber, chosenNumber);
+                    previousNumber = chosenNumber;
+                    entryHist.value += `${chosenNumber} ${event.target.value} `;
+                    chosenNumber = undefined;
+                    entry.value = total;
+                    previousOperator = currentOperator;
+                    currentOperator = event.target.value;
+                }
+
+            } else if(event.target.value !== "=" && chosenNumber !== undefined){
+                currentOperator = event.target.value;
+                previousOperator = currentOperator;
+                entryHist.value += `${chosenNumber} ${event.target.value} `;
+                previousNumber = chosenNumber;
+                chosenNumber = undefined;
+            }
+        }
+
+        if(entryHist.value.indexOf("/ 0") > 0){
+            clear(true);
+        }
+
+        
     });
+});
+
+negateButton.addEventListener("click", () => {
+    negate();
+})
+
+clearButton.addEventListener("click", () => {
+    clear();
+});
+
+backButton.addEventListener("click", () => {
+    back();
+});
+
+sqrtButton.addEventListener("click", () =>{
+    squareroot();
 });
 
 // FUNCTIONS
 
-function operator(operator, a, b){
-    if(operator === "plus"){
-        return add(a, b);
-    } else if(operator === "minus"){
-        return subtract(a, b);
-    } else if(operator === "multi"){
-        return multiply(a, b);
-    } else if(operator === "divide"){
-        return divide(a, b);
-    }
+function operation(operator, a, b){
+    if((a !== undefined) && (b !== undefined)){
+        if(operator === "+"){
+            return add(a, b);
+        } else if(operator === "-"){
+            return subtract(a, b);
+        } else if(operator === "x"){
+            return multiply(a, b);
+        } else if(operator === "/"){
+            return divide(a, b);
+        } else if(operator === "="){
+            return operation(previousOperator, a, b);
+        }
+    } 
 }
 
 function add(a, b){
-    return a + b;
+    let sum = Number(a) + Number(b);
+    if(sum.countDecimals() >= 4){
+        sum = sum.toFixed(3);
+    }
+    return sum;
 }
 
 function subtract(a, b){
-    return a - b;
+    let sum = Number(a) - Number(b);
+    if(sum.countDecimals() >= 4){
+        sum = sum.toFixed(3);
+    }
+    return sum;
 }
 
 function multiply(a, b){
-    return a * b;
+    let sum = Number(a) * Number(b);
+    if(sum.countDecimals() >= 4){
+        sum = sum.toFixed(3);
+    }
+    return sum;
 }
 
 function divide(a, b){
-    return a / b;
+    let sum = Number(a) / Number(b);
+    if(sum.countDecimals() >= 4){
+        sum = sum.toFixed(3);
+    }
+    return sum;
+}
+
+
+function squareroot(){
+    if(chosenNumber !== undefined){
+        chosenNumber = Math.sqrt(chosenNumber);
+        if(chosenNumber.countDecimals() >= 4){
+            chosenNumber = chosenNumber.toFixed(3);
+        }
+        entry.value = chosenNumber;
+    } else{
+        total = Math.sqrt(total);
+        if(total.countDecimals() >= 4){
+            total = total.toFixed(3);
+        }
+        entry.value = total;
+    }
+}
+
+function negate(){
+    if(chosenNumber !== undefined){
+        if(Number(chosenNumber) > 0){
+            chosenNumber = -Math.abs(chosenNumber);
+            entry.value = chosenNumber;
+        } else{
+            chosenNumber = Math.abs(chosenNumber);
+            entry.value = chosenNumber;
+        }
+    } else if(total === undefined && chosenNumber === undefined){
+        if(Number(previousNumber) > 0){
+            chosenNumber = -Math.abs(previousNumber);
+            entry.value = chosenNumber;
+        } else{
+            chosenNumber = Math.abs(previousNumber);
+            entry.value = chosenNumber;
+        }
+    } else{
+        if(Number(total) > 0){
+            total = -Math.abs(total);
+            entry.value = total;
+        } else{
+            total = Math.abs(total);
+            entry.value = total;
+        }
+    }
+
+}
+
+
+function clear(infinity){
+    if(infinity){
+        chosenNumber = 0;
+        previousNumber = undefined;
+        total = undefined;
+        currentOperator = undefined;
+        previousOperator = undefined;
+        entryHist.value = "";
+        entry.value = "cannot divide";
+    } else{
+        chosenNumber = 0;
+        previousNumber = undefined;
+        total = undefined;
+        currentOperator = undefined;
+        previousOperator = undefined;
+        entryHist.value = "";
+        entry.value = 0;
+    }
+
+}
+
+function back(){
+    if(typeof(chosenNumber) !== undefined && chosenNumber != "0"){
+        if(chosenNumber.length === 1){
+            chosenNumber = 0;
+        } else{
+            chosenNumber = chosenNumber.slice(0, -1);
+        }
+        entry.value = chosenNumber;
+    }
+    
 }
